ffc02430064d7a36c6cb952a89882d0dedec9d19
koungsakana52-boop <koungsakana52@gmail.com>
2026-01-30 14:45:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Grab the button element
+const button = document.getElementById('clickMeBtn');
+
+// Add event listener
+button.addEventListener('click', () => {
+  alert('Hello! Thanks for see my website.');
+});
